8316d0a32886148dbf6f452e143f12aafc7d9ced
Paul <pfvatterott@gmail.com>
2021-02-06 08:01:53 -0700

    Updated icons and color scheme

--- index.js
+++
@@ -1,8 +0,0 @@
-var options = [
-    {selector: '#staggered-test', offset: 200, callback: console.log('hey there') },
-    {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
-    {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
-    {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
-];
-Materialize.scrollFire(options);
-      
